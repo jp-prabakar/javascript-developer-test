2307eac1a898b7f4a94bfb0c8256373371473455
jp-prabakar <jp.prabakar@gmail.com>
2025-11-20 11:41:30 +1100

    Completed the test with working solutions by Prabakar

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,43 @@
-const { httpGet } = require('./mock-http-interface');
+"use strict";
+
+const { httpGet } = require("./mock-http-interface");
+
+const HTTP_STATUS_OK = 200;
+
+const parseResponseBody = (body) => {
+  try {
+    return JSON.parse(body);
+  } catch (e) {
+    return { message: "Invalid JSON response format" };
+  }
+};
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const fetchAllPromises = urls.map((url) => httpGet(url));
+
+  const allResults = await Promise.allSettled(fetchAllPromises);
+
+  const results = allResults.map((result) => {
+    if (result.status === "fulfilled") {
+      const httpResponse = result.value;
+
+      const parsedBody = parseResponseBody(httpResponse.body);
+
+      if (httpResponse.status === HTTP_STATUS_OK) {
+        return {
+          "Arnie Quote": parsedBody.message,
+        };
+      } else {
+        return {
+          FAILURE: parsedBody.message,
+        };
+      }
+    } else {
+      return { FAILURE: result.reason.message || "Request was rejected" };
+    }
+  });
+
+  return results;
 };
 
 module.exports = {
